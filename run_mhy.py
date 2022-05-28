7fc33dcce00c9b915a10f4165a3afc1985175791
Haiyang Mei <mhy666@mail.dlut.edu.cn>
2022-05-28 15:56:26 +0200

    Haiyang Mei --- 2022.05.22

--- run_mhy.py
+++ run_mhy.py
@@ -19,23 +19,37 @@ output_root = '/home/mhy/v2e/output/raw_demosaicing_polarization'
 check_mkdir(output_root)
 
 list = [
-    '00006',
-    '00104',
-    '00303',
-    '00404',
+    '00402',
+    '00403',
+    '00405',
+    '00502',
     '00503',
-    # '00704',
+    '00505',
+    '00803',
+    '00804',
     '00805',
+    '00903',
+    '00904',
+    '00905',
+    '01001',
+    '01002',
+    '01005',
+    '01303',
     '01304',
+    '01305',
+    '00702',
+    '00704',
+    '00705',
+    '01402',
     '01404',
-    '01505'
+    '01405',
 ]
 
 for name in list:
     input_dir = os.path.join(input_root, name)
     output_dir = os.path.join(output_root, name)
 
-    call_with_args = 'python v2e.py -i {} --polarization_input --input_frame_rate 25 --davis_output --dvs_h5 {}.h5 --dvs_aedat2 {}.aedat --dvs_text {}.txt --overwrite --timestamp_resolution=.005 --auto_timestamp_resolution=False --dvs_exposure duration 0.005 --output_folder={} --pos_thres=.08 --neg_thres=.08 --sigma_thres=0.02 --output_width=640 --output_height=480 --cutoff_hz=9 --start_time=1 --stop_time=9 --refractory_period 0.0002'.format(input_dir, name, name, name, output_dir)
+    call_with_args = 'python v2e.py -i {} --polarization_input --input_frame_rate 25 --davis_output --dvs_h5 {}.h5 --dvs_aedat2 {}.aedat --dvs_text {}.txt --overwrite --timestamp_resolution=.005 --auto_timestamp_resolution=False --dvs_exposure duration 0.005 --output_folder={} --pos_thres=.05 --neg_thres=.05 --sigma_thres=0.01 --output_width=640 --output_height=480 --cutoff_hz=9 --start_time=1 --stop_time=9 --refractory_period 0.0002'.format(input_dir, name, name, name, output_dir)
 
     print(call_with_args)
 
