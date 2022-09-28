62f7cc6f82983824441eac19736752ce06b39993
Haiyang Mei <mhy666@mail.dlut.edu.cn>
2022-09-28 15:26:36 +0200

    Haiyang Mei --- 2022.09.28

--- run_mhy.py
+++ run_mhy.py
@@ -14,8 +14,8 @@ import sys
 sys.path.append('..')
 from misc import check_mkdir
 
-input_root = '/home/mhy/data/movingcam/crop_davis640_demosaicing_5s'
-output_root = '/home/mhy/v2e/output/raw_demosaicing_polarization_5s'
+input_root = '~/data/movingcam/crop_davis640_demosaicing_5s'
+output_root = '~/v2e/output/raw_demosaicing_polarization_5s'
 check_mkdir(output_root)
 
 # list = [
@@ -57,7 +57,7 @@ for name in list:
     #     input_dir, name, name, name, output_dir)
 
     # gaussian-sampled threshold
-    threshold = torch.normal(0.18, 0.02, size=[1], dtype=torch.float32)[0]
+    threshold = torch.normal(0.12, 0.02, size=[1], dtype=torch.float32)[0]
     sigma = threshold * 0.1
     call_with_args = 'CUDA_VISIBLE_DEVICES=0 python v2e.py -i {} --polarization_input --input_frame_rate 25 --davis_output --dvs_h5 {}.h5 --dvs_aedat2 {}.aedat --dvs_text {}.txt --overwrite --timestamp_resolution=.0001 --auto_timestamp_resolution=False --dvs_exposure duration 0.005 --output_folder={} --pos_thres={} --neg_thres={} --sigma_thres={} --output_width=640 --output_height=480 --cutoff_hz=30 --refractory_period 0.00003 --batch_size 64'.format(input_dir, name, name, name, output_dir, threshold, threshold, sigma)
 
